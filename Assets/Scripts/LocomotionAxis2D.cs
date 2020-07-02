405ff53835fe522a2d6f650a203b477ba3028c3b
samandalib <hesam.andalib@gmail.com>
2020-07-02 18:25:24 -0500

    Base with Locomotion, Rotation, Teleportation functionalities is ready

--- Assets/Scripts/LocomotionAxis2D.cs
+++ Assets/Scripts/LocomotionAxis2D.cs
@@ -5,38 +5,32 @@ using OVR;
 
 public class LocomotionAxis2D : MonoBehaviour
 {
-    [SerializeField]
-    private Rigidbody Player;
-    public float speed;
-    private Vector2 _controller;
-    
-    public enum ControllerChoice
-    {
-        Left_Thumbstick,
-        Right_Thumbstick
-    }
-    public ControllerChoice currentChoice;
-    // Start is called before the first frame update
-    void Start()
-    {
-        switch(currentChoice)
-        {
-            case ControllerChoice.Left_Thumbstick:
-                _controller = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick, OVRInput.Controller.LTouch);
-                break;
-            case ControllerChoice.Right_Thumbstick:
-                _controller = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick, OVRInput.Controller.RTouch);
-                break;
-        }
-    }
 
+    public float speed;
+
+    //set the gameobject that you want to control, if it is the player, put "PlayAreaAlias" in this field
+    [SerializeField]
+    private GameObject Target;
+    
     // Update is called once per frame
     void Update()
     {
-        float fixedY = Player.transform.position.y;
+        if (OVRInput.Get(OVRInput.Touch.PrimaryThumbstick))//Boolean: check to see if the Thumbstick on selected controller is touched
+        {
+            
+            //get the value of the Vector2 of Axis2D from the controller (the value for each is between -1 and 1)
+            Vector2 displacement = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+            float Xdisplacement = displacement.x;
+            float Zdisplacement = displacement.y;
+            
+            
+            //set the x and z to the values from Thumbstick
+            Target.transform.Translate(new Vector3(Xdisplacement, 0, Zdisplacement) * Time.deltaTime * speed);
+
+        }
+        //float fixedY = Player.transform.position.y;
 
 
-        Player.transform.position += (transform.right * _controller.x + transform.forward * _controller.y) * Time.deltaTime * speed;
-        Player.transform.position = new Vector3(Player.transform.position.x, fixedY, Player.transform.position.z);
+        
     }
 }
---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+
+
+    //set the gameobject that you want to control, if it is the player, put "PlayAreaAlias" in this field
+    [SerializeField]
+    private GameObject Target;
+
+    [SerializeField]
+    private int _rotationSpeed = 30;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Touch.SecondaryThumbstick))//Boolean: check to see if the Thumbstick on selected controller is touched
+        {
+            //get the value of the Vector2 of Axis2D from the controller (the value for each is between -1 and 1)
+            Vector2 displacement = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+            float Xdisplacement = displacement.x;//we can only use one value for rotation
+
+            //To make it more clear which axis we are going to rotate around
+            float YRotation = Xdisplacement;
+            
+            //RotateAround(the point where we want the rotation happens in, the Axis we want the rotation happen around, the speed or degree of rotation)
+            Target.transform.RotateAround(Target.transform.position, new Vector3(0, YRotation, 0) , _rotationSpeed * Time.deltaTime);
+        }
+        
+
+
+
+    }
+}
